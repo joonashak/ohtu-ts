1e36ff7c589b9196becf959db4fb0d12a881b8a6
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2019-12-06 10:49:30 +0200

    Revert "Fix bad/nonexistent output on windows (still experimental)"
    This reverts commit 5e4e2381db7196252fe1b9877bea4277ceb28810.

--- src/main/java/ohtu/ts/ui/Terminal.java
+++ src/main/java/ohtu/ts/ui/Terminal.java
@@ -8,7 +8,6 @@ package ohtu.ts.ui;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 /**
  *
@@ -111,10 +110,9 @@ public class Terminal {
         if (retVal != 0) {
             throw new Exception("Abnormal command line");
         }
-        // System.out.println(Arrays.deepToString(lines));
         reader.close();
-        return new int[]{Integer.parseInt(lines[1].split(":")[1].trim()) - 1,
-            Integer.parseInt(lines[0].split(":")[1].trim()) - 1};
+        return new int[]{Integer.parseInt(lines[0].split(":")[1],
+            Integer.parseInt(lines[1].split(":")[1]))};
     }
 
 }
